9e4b45003e4da26c853adb43fecdf6d5643ed13b
Afonso Castelão <afonso.castelao@hotmail.com>
2023-07-22 02:07:11 +0100

    Updated Code Formatter

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 require('dotenv').config();
-const express =  require('express');
+const express = require('express');
 const cors = require('cors');
 const { Configuration, OpenAIApi } = require('openai');
-const { generatePrompt, formatData, sendToGDrive } = require('./src/appService.ts');
+const {
+    generatePrompt,
+    formatData,
+    sendToGDrive,
+} = require('./src/appService.ts');
 
 // App Init
 let app = express();
@@ -17,16 +21,18 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // Main Endpoint
-app.post("/request/:fileName", async (req: any, res: any) => {
-
-    const fileTitle = req.params.fileName
-    console.log('> Requested to build Mind Map on file \'' + fileTitle + '.csv\'...')
+app.post('/request/:fileName', async (req: any, res: any) => {
+    const fileTitle = req.params.fileName;
+    console.log(
+        "> Requested to build Mind Map on file '" + fileTitle + ".csv'..."
+    );
 
     if (!configuration.apiKey) {
         res.status(500).json({
             error: {
-                message: "OpenAI API key not configured, please follow instructions in README.md",
-            }
+                message:
+                    'OpenAI API key not configured, please follow instructions in README.md',
+            },
         });
         return;
     }
@@ -35,35 +41,34 @@ app.post("/request/:fileName", async (req: any, res: any) => {
     if (inputText.trim().length === 0) {
         res.status(400).json({
             error: {
-                message: "Please enter a valid input",
-            }
+                message: 'Please enter a valid input',
+            },
         });
         return;
     }
 
     try {
-
         // OpenAI text completion call
         const completion = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: 'text-davinci-003',
             prompt: generatePrompt(inputText),
             temperature: 0.6,
-            max_tokens: 1500
+            max_tokens: 1500,
         });
 
         // Pass result to a JSON format
         const myResult = JSON.parse(completion.data.choices[0].text);
 
         // Format into csv file data
-        const formattedCSV = formatData(myResult, fileTitle)
+        const formattedCSV = formatData(myResult, fileTitle);
 
         // Save and send file to Google Drive
-        sendToGDrive(fileTitle, formattedCSV)
+        sendToGDrive(fileTitle, formattedCSV);
         // TODO: Maybe implement error handling logic here ??
 
         // Return data to FE for table rendering
         res.status(200).json({
-            result: myResult
+            result: myResult,
         });
     } catch (error) {
         // Consider adjusting the error handling logic for your use case
@@ -75,11 +80,11 @@ app.post("/request/:fileName", async (req: any, res: any) => {
             res.status(500).json({
                 error: {
                     message: 'An error occurred during your request.',
-                }
+                },
             });
         }
     }
-})
+});
 
 // Initiate listening
 app.listen(port, () => {
--- src/appService.ts
+++ src/appService.ts
@@ -3,175 +3,192 @@ const path = require('path');
 const fs = require('fs');
 
 export async function sendToGDrive(fileTitle: string, fileData: Array<any>) {
-
-    const fileName = fileTitle + '.csv'
+    const fileName = fileTitle + '.csv';
 
     // Save file and then send to GDrive
-    fs.writeFile(path.resolve(__dirname, '../public/' + fileName), fileData, async (err: any) => {
-        
-        // If there was an error, stop here
-        if (err) { 
-            console.log(err) 
-        }
-        else {
+    fs.writeFile(
+        path.resolve(__dirname, '../public/' + fileName),
+        fileData,
+        async (err: any) => {
+            // If there was an error, stop here
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('> Saved ' + fileName + '!');
 
-            console.log('> Saved ' + fileName + '!')
+                const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || '';
+                const driveClientSecret =
+                    process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
+                const driveRedirectUri =
+                    process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
+                const driveRefreshToken =
+                    process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
 
-            const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || '';
-            const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
-            const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
-            const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
+                const googleDriveService = new GoogleDriveService(
+                    driveClientId,
+                    driveClientSecret,
+                    driveRedirectUri,
+                    driveRefreshToken
+                );
 
-            const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+                const finalPath = path.resolve(
+                    __dirname,
+                    '../public/' + fileName
+                );
+                const folderName = 'Tese';
 
-            const finalPath = path.resolve(__dirname, '../public/' + fileName);
-            const folderName = 'Tese';
+                if (!fs.existsSync(finalPath)) {
+                    throw new Error('File not found!');
+                }
 
-            if (!fs.existsSync(finalPath)) {
-                throw new Error('File not found!');
+                let folder = await googleDriveService
+                    .searchFolder(folderName)
+                    .catch((error) => {
+                        console.error(error);
+                        return null;
+                    });
+
+                if (!folder) {
+                    folder = await googleDriveService.createFolder(folderName);
+                }
+
+                // Sending csv file
+                return await googleDriveService
+                    .saveFile(fileName, finalPath, 'application/csv', folder.id)
+                    .then(() => {
+                        console.info(
+                            'File uploaded into Google Drive successfully!\n'
+                        );
+
+                        // Delete the file on the server
+                        // fs.unlinkSync(finalPath);
+                    })
+                    .catch((error: any) => {
+                        console.error(error);
+                        return;
+                    });
             }
-
-            let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
-                console.error(error);
-                return null;
-            });
-
-            if (!folder) {
-                folder = await googleDriveService.createFolder(folderName);
-            }
-
-            // Sending csv file
-            return await googleDriveService.saveFile(fileName, finalPath, 'application/csv', folder.id).then(() => {
-                console.info('File uploaded into Google Drive successfully!\n');
-
-                // Delete the file on the server
-                // fs.unlinkSync(finalPath);
-            }).catch((error: any) => {
-                console.error(error);
-                return
-            });
-
         }
-    });    
+    );
 }
 
 export function generatePrompt(inputText: string) {
-
-      return `
+    return `
       Please create a Mind Map on a .csv file format that will be imported into Noda.io. Each Node Name field should have a maximum of 6 words. The Mind Map should summarize the following text:
       "${inputText}"
 
       The response format should be only an array of objects describing each node, as a json format, without any words or newlines before the array.
       Example output:
-      [{"NodeId": "value", "NomeName": "value", "FromNode": "value", "NodeLevel": "value"}, {"NodeId": "value", "NomeName": "value", "FromNode": "value", "NodeLevel": "value"}]`
+      [{"NodeId": "value", "NomeName": "value", "FromNode": "value", "NodeLevel": "value"}, {"NodeId": "value", "NomeName": "value", "FromNode": "value", "NodeLevel": "value"}]`;
 }
 
 export function formatData(dataRows: Array<object>, title: string) {
-
     // TODO: Map spatial params here
 
-    let linkList = []
-    let finalData = []
-    let linkId = 0
+    let linkList = [];
+    let finalData = [];
+    let linkId = 0;
 
     // Format Node Rows and Create Links
     dataRows.forEach((element: any) => {
-
         // Populate Link List
         if (+element.FromNode > 0) {
-            linkList.push([element.FromNode, element.NodeId])
+            linkList.push([element.FromNode, element.NodeId]);
         }
 
         let nodeObj = {
-            "Uuid": "",
-            "Title": "",
-            "Notes": "",
-            "ImageURL": "",
-            "PageURL": "",
-            "Color": "",
-            "Opacity": 1,
-            "Shape": "Ball",
-            "Size": "",
-            "PositionX": "",
-            "PositionY": "",
-            "PositionZ": "",
-            "Collapsed": "No",
-            "Type": "",
-            "FromUuid": "",
-            "ToUuid": ""
-        }
-        nodeObj.Uuid = element.NodeId
-        nodeObj.Title = element.NodeName
+            Uuid: '',
+            Title: '',
+            Notes: '',
+            ImageURL: '',
+            PageURL: '',
+            Color: '',
+            Opacity: 1,
+            Shape: 'Ball',
+            Size: '',
+            PositionX: '',
+            PositionY: '',
+            PositionZ: '',
+            Collapsed: 'No',
+            Type: '',
+            FromUuid: '',
+            ToUuid: '',
+        };
+        nodeObj.Uuid = element.NodeId;
+        nodeObj.Title = element.NodeName;
 
         // Assign Size and Colours to Nodes according to Node Level
         switch (element.NodeLevel) {
-            
             case 1:
-                nodeObj.Size = '15'
-                nodeObj.Color = 'E91E63'
-                nodeObj.Shape = 'Box'
-                break
-            
+                nodeObj.Size = '15';
+                nodeObj.Color = 'E91E63';
+                nodeObj.Shape = 'Box';
+                break;
+
             case 2:
-                nodeObj.Size = '9.5'
-                nodeObj.Color = 'FFEB3B'
-                break
+                nodeObj.Size = '9.5';
+                nodeObj.Color = 'FFEB3B';
+                break;
 
             case 3:
-                nodeObj.Size = '7.5'
-                nodeObj.Color = '4CAF50'
-                break
-            
+                nodeObj.Size = '7.5';
+                nodeObj.Color = '4CAF50';
+                break;
+
             case 4:
-                nodeObj.Size = '6.5'
-                nodeObj.Color = '00BCD4'
-                break
+                nodeObj.Size = '6.5';
+                nodeObj.Color = '00BCD4';
+                break;
 
             default:
-                nodeObj.Size = '5'
-                nodeObj.Color = 'F44336'
+                nodeObj.Size = '5';
+                nodeObj.Color = 'F44336';
         }
 
-        finalData.push(nodeObj)
+        finalData.push(nodeObj);
     });
 
     // Starting Id for links
-    linkId = finalData.length + 1
+    linkId = finalData.length + 1;
 
     // Push links into final data
-    linkList.forEach(el => {
+    linkList.forEach((el) => {
         finalData.push({
-            "Uuid": JSON.stringify(linkId),
-            "Title": "",
-            "Notes": "",
-            "ImageURL": "",
-            "PageURL": "",
-            "Color": '171717',
-            "Opacity": 1,
-            "Shape": 'Solid',
-            "Size": 1,
-            "PositionX": "",
-            "PositionY": "",
-            "PositionZ": "",
-            "Collapsed": "",
-            "Type": "",
-            "FromUuid": el[0],
-            "ToUuid": el[1]
-        })
-        
-        linkId += 1
+            Uuid: JSON.stringify(linkId),
+            Title: '',
+            Notes: '',
+            ImageURL: '',
+            PageURL: '',
+            Color: '171717',
+            Opacity: 1,
+            Shape: 'Solid',
+            Size: 1,
+            PositionX: '',
+            PositionY: '',
+            PositionZ: '',
+            Collapsed: '',
+            Type: '',
+            FromUuid: el[0],
+            ToUuid: el[1],
+        });
+
+        linkId += 1;
     });
 
     // Format to CSV
-    let fields = Object.keys(finalData[0])
-    let replacer = function(key: any, value: any) { return value === null ? '' : value } 
-    let csvData: any = finalData.map(function(row){
-      return fields.map(function(fieldName){
-        return JSON.stringify(row[fieldName], replacer)
-      }).join(',')
-    })
-    csvData.unshift(fields.join(',')) // Add header column
+    let fields = Object.keys(finalData[0]);
+    let replacer = function (key: any, value: any) {
+        return value === null ? '' : value;
+    };
+    let csvData: any = finalData.map(function (row) {
+        return fields
+            .map(function (fieldName) {
+                return JSON.stringify(row[fieldName], replacer);
+            })
+            .join(',');
+    });
+    csvData.unshift(fields.join(',')); // Add header column
     csvData = csvData.join('\r\n');
 
-    return csvData
+    return csvData;
 }
